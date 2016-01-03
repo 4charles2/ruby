9b7ef9a8a49df9b176aaf6069f561642f8548383
4charles2 <ctognol@laposte.net>
2016-01-03 15:32:54 +0100

    boucle avec une conditon dans la lecture du tableau

--- conditionTableau.rb
+++ conditionTableau.rb
@@ -2,8 +2,18 @@ emails = [
 	"bob@bob.com","jane@hello.com"
 ]
 
+emails.each do |email|
+	puts email
+	if email == "bob@bob.com"
+		puts "bonjour bob!"
+	else
+		puts "bonjour jane d;-}"
+	end
+end
+
+
 if emails.size == 1
 	puts "J'ai un email"
 else
-	puts "J'ai plus ou moins d'un email"
+	puts "J'ai plus ou moins d'un email ?"
 end
