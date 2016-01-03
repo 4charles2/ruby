31d9d5855f700d38cc662ea0eb9ab5f6d00e036a
4charles2 <ctognol@laposte.net>
2016-01-03 17:02:07 +0100

    creatin d'une autre methode

--- methode.rb
+++ methode.rb
@@ -1,7 +1,12 @@
 class Utilisateur
-	attr_accessor :prenom, :nom
+	attr_accessor :prenom, :nom, :pays
+
 	def nom_complet
-		prenom + " " + nom
+		prenom + " " + nom + " " + pays
+	end
+
+	def habite_en(valeur)
+		valeur == pays
 	end
 end
 
@@ -10,6 +15,12 @@ end
 bob = Utilisateur.new
 bob.prenom = "Bob"
 bob.nom = "Lenon"
+bob.pays = "France"
 
 puts bob.nom_complet
 
+puts "Est ce que bob habite en France?"
+if bob.habite_en("France")
+	puts "Bonjour cher ami France"
+else
+	puts "Tu n'est pas France"
